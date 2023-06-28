fe1f887e1c2f831987e66eda3ac500b2e042da83
Kuesung Park <gueit214@naver.com>
2023-06-28 21:52:33 +0900

    refactor : Input 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Input() {
   return (
     <div className='border-main relative h-[3rem] w-full rounded-full border'>
-      <input className='h-full  w-full rounded-full' />
+      <input className='h-full  w-full rounded-full px-4 outline-none' />
       <div className='absolute inset-y-0 -right-1  my-auto mr-2 flex h-[2.625rem] w-[2.625rem] items-center justify-center rounded-full bg-[#F2F6F5]'>
-        <Image src='/svg/arrow_back.svg' alt='arrow' width={30} height={30} />
+        <Image src='/svg/arrow-up.svg' alt='arrow' width={30} height={30} />
       </div>
     </div>
   );
