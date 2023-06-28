da07a306466fcc3c375d1f1bfe3e70b37f1ec02b
Kuesung Park <gueit214@naver.com>
2023-06-28 20:03:51 +0900

    feat : 첫 화면 구현

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,7 +3,7 @@ interface ButtonProps {
 }
 export default function Button({ text }: ButtonProps) {
   return (
-    <button className='bg-main h-[3.75rem] w-full rounded-[1rem] text-[1.25rem] text-white'>
+    <button className='bg-main h-[3.75rem] w-full rounded-[1rem] text-[1.25rem] text-white '>
       {text}
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Button from '@/components/buttons/Button';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='relative flex h-full items-center justify-center'>
+      <div>Logo</div>
+      <div className='fixed bottom-5 left-0 right-0 m-auto w-[25rem]'>
+        <Button text='모바일 통지서 연결하기' />
+      </div>
     </div>
   );
 }
