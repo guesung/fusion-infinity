33b05302c27c508bd6ea1246fafae2dd256fda47
Kuesung Park <gueit214@naver.com>
2023-06-28 20:37:29 +0900

    refactor : 색 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from 'next/router';
+
 import Button from '@/components/buttons/Button';
 
 export default function HomePage() {
+  const router = useRouter();
   return (
-    <div className='relative flex h-full items-center justify-center'>
+    <div className='relative flex h-full items-center justify-center bg-white'>
       <div>Logo</div>
       <div className='fixed bottom-5 left-0 right-0 m-auto w-[25rem]'>
-        <Button text='모바일 통지서 연결하기' />
+        <Button
+          text='모바일 통지서 연결하기'
+          onClick={() => {
+            router.push('/page2');
+          }}
+        />
       </div>
     </div>
   );
