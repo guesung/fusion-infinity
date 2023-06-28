feebd41ca9b8cdb2943fbf03ccbd03f477821cd1
Kuesung Park <gueit214@naver.com>
2023-06-28 20:42:15 +0900

    feat : Button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,9 +1,13 @@
 interface ButtonProps {
   text: string;
+  onClick: () => void;
 }
-export default function Button({ text }: ButtonProps) {
+export default function Button({ text, ...rest }: ButtonProps) {
   return (
-    <button className='bg-main h-[3.75rem] w-full rounded-[1rem] text-[1.25rem] text-white '>
+    <button
+      className='bg-main h-[3.75rem] w-full rounded-[1rem] text-[1.25rem] text-white'
+      {...rest}
+    >
       {text}
     </button>
   );
